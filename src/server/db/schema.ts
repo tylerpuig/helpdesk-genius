26a5ed45402899ff601770783b9213468e867832
tylerpuig <tphockey96@yahoo.com>
2025-01-19 16:14:28 -0600

    rm deprecated drizzle syntax

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { relations, sql } from "drizzle-orm";
+import { relations, sql } from 'drizzle-orm'
 import {
   index,
   integer,
@@ -7,116 +7,103 @@ import {
   text,
   timestamp,
   varchar,
-  pgTable,
-} from "drizzle-orm/pg-core";
-import { type AdapterAccount } from "next-auth/adapters";
+  pgTable
+} from 'drizzle-orm/pg-core'
+import { type AdapterAccount } from 'next-auth/adapters'
 
 export const posts = pgTable(
-  "post",
+  'post',
   {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("created_by", { length: 255 })
+    id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar('name', { length: 256 }),
+    createdById: varchar('created_by', { length: 255 })
       .notNull()
       .references(() => users.id),
-    createdAt: timestamp("created_at", { withTimezone: true })
+    createdAt: timestamp('created_at', { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date(),
-    ),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date())
   },
-  (example) => ({
-    createdByIdIdx: index("created_by_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
+  (example) => [index('created_by_idx').on(example.createdById), index('name_idx').on(example.name)]
+)
 
-export const users = pgTable("user", {
-  id: varchar("id", { length: 255 })
+export const users = pgTable('user', {
+  id: varchar('id', { length: 255 })
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("email_verified", {
-    mode: "date",
-    withTimezone: true,
+  name: varchar('name', { length: 255 }),
+  email: varchar('email', { length: 255 }).notNull(),
+  emailVerified: timestamp('email_verified', {
+    mode: 'date',
+    withTimezone: true
   }).default(sql`CURRENT_TIMESTAMP`),
-  image: varchar("image", { length: 255 }),
-});
+  image: varchar('image', { length: 255 })
+})
 
 export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
-}));
+  accounts: many(accounts)
+}))
 
 export const accounts = pgTable(
-  "account",
+  'account',
   {
-    userId: varchar("user_id", { length: 255 })
+    userId: varchar('user_id', { length: 255 })
       .notNull()
       .references(() => users.id),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("provider_account_id", {
-      length: 255,
+    type: varchar('type', { length: 255 }).$type<AdapterAccount['type']>().notNull(),
+    provider: varchar('provider', { length: 255 }).notNull(),
+    providerAccountId: varchar('provider_account_id', {
+      length: 255
     }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: varchar('token_type', { length: 255 }),
+    scope: varchar('scope', { length: 255 }),
+    id_token: text('id_token'),
+    session_state: varchar('session_state', { length: 255 })
   },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
+  (account) => [
+    primaryKey({
+      columns: [account.provider, account.providerAccountId]
     }),
-    userIdIdx: index("account_user_id_idx").on(account.userId),
-  }),
-);
+    index('account_user_id_idx').on(account.userId)
+  ]
+)
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
+  user: one(users, { fields: [accounts.userId], references: [users.id] })
+}))
 
 export const sessions = pgTable(
-  "session",
+  'session',
   {
-    sessionToken: varchar("session_token", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("user_id", { length: 255 })
+    sessionToken: varchar('session_token', { length: 255 }).notNull().primaryKey(),
+    userId: varchar('user_id', { length: 255 })
       .notNull()
       .references(() => users.id),
-    expires: timestamp("expires", {
-      mode: "date",
-      withTimezone: true,
-    }).notNull(),
+    expires: timestamp('expires', {
+      mode: 'date',
+      withTimezone: true
+    }).notNull()
   },
-  (session) => ({
-    userIdIdx: index("session_user_id_idx").on(session.userId),
-  }),
-);
+  (session) => [index('session_user_id_idx').on(session.userId)]
+)
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
+  user: one(users, { fields: [sessions.userId], references: [users.id] })
+}))
 
 export const verificationTokens = pgTable(
-  "verification_token",
+  'verification_token',
   {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", {
-      mode: "date",
-      withTimezone: true,
-    }).notNull(),
+    identifier: varchar('identifier', { length: 255 }).notNull(),
+    token: varchar('token', { length: 255 }).notNull(),
+    expires: timestamp('expires', {
+      mode: 'date',
+      withTimezone: true
+    }).notNull()
   },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  }),
-);
+  (vt) => [primaryKey({ columns: [vt.identifier, vt.token] })]
+)
