fc31527f81aad397d779924b044d83c8e9ba8583
tylerpuig <tphockey96@yahoo.com>
2025-01-19 15:11:16 -0600

    install dashboard

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  semi: false,
+  printWidth: 100,
+  trailingComma: "none",
 };
